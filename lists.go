6bebd25805e96895c3886b0a30fbaed567ec2481
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-01 17:22:58 -0300

    selecting parts of array with slices

--- lists.go
+++ lists.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var productNames [4]string = [4]string{"A book"}
+	//var productNames [4]string = [4]string{"A book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
-	productNames[2] = "A carpet"
-	fmt.Println(productNames)
+	//productNames[2] = "A carpet"
+	//fmt.Println(productNames)
 	fmt.Println(prices[2])
+
+	featuredPrice := prices[1:3]
+	fmt.Println(featuredPrice)
 
 }
