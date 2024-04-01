473bcd5aee3e89fc071516905bb7213200583551
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-03-31 23:18:58 -0300

    working with arrys

--- lists.go
+++ lists.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	var productNames [4]string = [4]string{"A book"}
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
+	productNames[2] = "A carpet"
+	fmt.Println(productNames)
+	fmt.Println(prices[2])
+
 }
