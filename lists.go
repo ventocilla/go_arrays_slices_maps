7ad84acdaceab3b7fdc27230cb6425eb703eebe5
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-02 17:49:52 -0300

    unpacking list values

--- lists.go
+++ lists.go
@@ -7,11 +7,14 @@ func main() {
 	prices := []float64{10.99, 8.99}
 	fmt.Println(prices)
 	//fmt.Println(prices[:1])
-	prices = append(prices, 5.99)
+	prices = append(prices, 5.99, 12.99, 29.99, 100.0)
 	fmt.Println(prices)
 	prices = prices[1:]
 	fmt.Println(prices)
 
+	discontPrices := []float64{101.99, 80.99, 20.5}
+	prices = append(prices, discontPrices...)
+	fmt.Println(prices)
 	/*
 		var productNames [4]string = [4]string{"A book"}
 		prices := [4]float64{10.99, 9.99, 45.99, 20.0}
