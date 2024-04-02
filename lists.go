4aef4bd89536700a0aa88f8c5a42664366dedf38
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-01 21:03:23 -0300

    building dynamic list with slices

--- lists.go
+++ lists.go
@@ -3,18 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	//var productNames [4]string = [4]string{"A book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+
+	prices := []float64{10.99, 8.99}
 	fmt.Println(prices)
-	//productNames[2] = "A carpet"
-	//fmt.Println(productNames)
-	//fmt.Println(prices[2])
-	featuredPrice := prices[1:]
-	fmt.Println(featuredPrice)
-	highlitedPrices := featuredPrice[:1]
-	featuredPrice[0] = 199.99
-	fmt.Println(highlitedPrices)
-	fmt.Println(len(featuredPrice), cap(featuredPrice))
+	//fmt.Println(prices[:1])
+	prices = append(prices, 5.99)
+	fmt.Println(prices)
+	prices = prices[1:]
 	fmt.Println(prices)
 
+	/*
+		var productNames [4]string = [4]string{"A book"}
+		prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+		fmt.Println(prices)
+		productNames[2] = "A carpet"
+		fmt.Println(productNames)
+		fmt.Println(prices[2])
+		featuredPrice := prices[1:]
+		fmt.Println(featuredPrice)
+		highlitedPrices := featuredPrice[:1]
+		featuredPrice[0] = 199.99
+		fmt.Println(highlitedPrices)
+		fmt.Println(len(featuredPrice), cap(featuredPrice))
+		fmt.Println(prices)
+	*/
 }
